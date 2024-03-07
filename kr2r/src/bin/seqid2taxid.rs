bfd559d339c036ac6b680d27414a6f1561d9e9e0
eric <eric9n@gmail.com>
2024-03-07 21:49:07 +0800

    seqid to taxid

---
+++ kr2r/src/bin/seqid2taxid.rs
@@ -0,0 +1,31 @@
+use clap::Parser;
+
+use kr2r::utils::{create_seqid2taxid_file, summary_prelim_map_files};
+use std::io::Result;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(version, about = "seqid to taxid map file")]
+pub struct Args {
+    /// the database directory
+    #[arg(long, required = true)]
+    pub source: PathBuf,
+
+    /// seqid2taxid.map file path, default = $source/seqid2taxid.map
+    #[arg(short = 'f', long)]
+    map_file: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let prelim_file = summary_prelim_map_files(&args.source)?;
+
+    let map_file = if let Some(seqid_file) = args.map_file {
+        seqid_file
+    } else {
+        args.source.join("seqid2taxid.map")
+    };
+    create_seqid2taxid_file(prelim_file, map_file)?;
+
+    Ok(())
+}
--- kr2r/src/utils.rs
+++ kr2r/src/utils.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
-use std::path::Path;
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader, Result, Write};
+use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 /// 读取 seqid2taxid.map 文件。为了裁剪 ncbi 的 taxonomy 树
@@ -76,4 +76,51 @@ pub fn find_library_fna_files<P: AsRef<Path>>(path: P) -> Vec<String> {
         .filter(|e| e.path().file_name() == Some("library.fna".as_ref()))
         .map(|e| e.path().to_string_lossy().into_owned())
         .collect()
+}
+
+pub fn summary_prelim_map_files<P: AsRef<Path>>(data_dir: P) -> Result<PathBuf> {
+    let lib_path = data_dir.as_ref().join("library");
+
+    let prelim_map_files: Vec<PathBuf> = WalkDir::new(&lib_path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().file_name() == Some("prelim_map.txt".as_ref()))
+        .map(|e| e.path().to_path_buf())
+        .collect();
+
+    let summary_file_path = data_dir.as_ref().join("prelim_map.txt");
+    if summary_file_path.exists() && summary_file_path.is_file() {
+        // 删除文件
+        fs::remove_file(&summary_file_path)?;
+    }
+
+    let mut summary_file = File::create(&summary_file_path)?;
+    // 遍历找到的文件并汇总内容
+    for path in prelim_map_files {
+        let file = File::open(path)?;
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            let line = line?;
+            writeln!(summary_file, "{}", line)?;
+        }
+    }
+
+    Ok(summary_file_path)
+}
+
+pub fn create_seqid2taxid_file<P: AsRef<Path>>(prelim_map_file: P, output_file: P) -> Result<()> {
+    let file = File::open(prelim_map_file)?;
+    let reader = BufReader::new(file);
+    let mut output = File::create(output_file)?;
+
+    for line in reader.lines() {
+        let line = line?;
+        let columns: Vec<&str> = line.split('\t').collect();
+        if columns.len() >= 3 {
+            // 写入第二列和第三列到输出文件
+            writeln!(output, "{}\t{}", columns[1], columns[2])?;
+        }
+    }
+
+    Ok(())
 }
